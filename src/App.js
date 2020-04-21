bb17f601081d6acfdd073dc37c6220ba38509545
Lucas <lucas.capanelli.es@gmail.com>
2020-04-21 16:57:00 -0300

    New user card component

--- src/App.js
+++ src/App.js
@@ -2,53 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//card
-//list
+const testData = [
+  {
+    name: "Lucas", 
+    company: "Github",
+    userName: "LCapanelli", 
+    avatar_url: "https://avatars2.githubusercontent.com/u/771530?s=60&v=4", 
+    profile_url: "https://github.com/LCapanelli"
+}
+]; 
+
+const CardList = (props) => (
+  <div>
+    { testData.map(profile => <Card { ...profile }/>) }
+  </div>
+);
 
 class Card extends React.Component {
   render() {
+    const profile = this.props; 
     return (
       <div className="Github-profile">
-        <img className="Profile-image" src="https://placehold.it/75" />
+        <img className="Profile-image" src={ profile.avatar_url } />
         <div className="Info">
-          <div className="Info.Name">name here</div>
-          <div className="Info.Company">company here</div>
+          <div className="Info-name">{ profile.name }</div>
+          <a className="Info-userName" href={ profile.profile_url }>{ profile.userName }</a>
+          <div className="Info-company">{ profile.company }</div>
         </div>
       </div>
       ); 
   }
-}
+}; 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <div className="Header">{this.props.title}</div>
-        <Card />
+        <div className="Header">{ this.props.title }</div>
+        <CardList />
       </div>
     ); 
   }
-}
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
+}; 
 
 export default App;
